65b7d8c2cc42429acbe6e4374e0f980eae2792da
TorchKat <88567332+TorchKat@users.noreply.github.com>
2023-02-09 13:02:19 -0500

    Laptop Sync

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,10 @@
+const country = 'Ferelden';
+const continent = 'Thedas';
+let population = 912125;
+const isIsland = false;
+const language = 'Trade Toungue';
+
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,61 @@
+/*
+let js ='amazing';
+console.log(40+8+23-10);
+
+console.log("Karic");
+console.log(19);
+
+let firstName = "Evelyn";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let alistair_evelyn = "AK";
+let $function = 24;
+
+let person = "Alistair";
+let PI = 3.1415;
+
+let myFirstJob = "Noble";
+let myCurrentJob = "Grey Warden";
+
+let job1 = "royal";
+let job2 = "warrior";
+
+console.log(myFirstJob);
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+//console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+//console.log(typeof 19);
+//console.log(typeof 'Alistair');
+
+javaScriptIsFun = 'YES!';
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2023;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+let age = 19;
+age = 20;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = 'Royal';
+job = 'Grey Warden';
+
+lastName = 'Cousland';
+console.log(lastName);
